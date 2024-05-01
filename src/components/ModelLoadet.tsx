bb5fb80ea7d5e3fd044234a65dd2c33084028c24
Chinmay-Raiker <chinmay.r@consultadd.com>
2024-05-01 13:00:20 +0530

    feat(modelLoader): added glb model in the background

---
+++ src/components/ModelLoadet.tsx
@@ -0,0 +1,26 @@
+import React, { Suspense } from "react";
+import { OrbitControls, Loader } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { useLoader } from "@react-three/fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader.js";
+
+export default function ModelLoader() {
+    const model = useLoader(GLTFLoader, "./space_dreamscape.glb", (loader) => {
+        const dracoLoader = new DRACOLoader();
+        dracoLoader.setDecoderPath("./draco/");
+        loader.setDRACOLoader(dracoLoader);
+    });
+
+    console.log(model);
+    return (
+        <Canvas className="absolute left-0 top-0 -z-10">
+            <Suspense fallback={<Loader />}>
+                <ambientLight intensity={1.5} />
+                <directionalLight position={[10, 10, 10]} />
+                <primitive object={model.scene} />
+                <OrbitControls />
+            </Suspense>
+        </Canvas>
+    );
+}
