c6d6405c548053e2bac9ef6d5d24aaf0034b7a37
suryamjain <suryam.j@consultadd.com>
2024-05-24 08:04:34 +0530

    refactor: removed axis-helper from OceanModel

--- src/components/OceanModel.tsx
+++ src/components/OceanModel.tsx
@@ -1,11 +1,11 @@
 import { useGLTF } from "@react-three/drei";
-import { AxesHelper } from "three";
 
 export default function OceanModel() {
     const { nodes } = useGLTF("./ocean.glb");
 
-    return <>
-        <mesh geometry={nodes.Ocean.geometry} material={nodes.Ocean.material} rotation={[-Math.PI / 2, 0, 0]} />
-        <primitive object={new AxesHelper(1)} />
-    </>;
+    return <mesh
+        geometry={nodes.Ocean.geometry}
+        material={nodes.Ocean.material}
+        rotation={[-Math.PI / 2, 0, 0]}
+    />;
 }
