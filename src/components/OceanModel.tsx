d30d6b414f933894abacdfe0bb54d56742c5e6c2
suryamjain <suryam.j@consultadd.com>
2024-05-24 09:10:06 +0530

    refactor(ocean): changed mesh into primitive

--- src/components/OceanModel.tsx
+++ src/components/OceanModel.tsx
@@ -2,11 +2,9 @@ import { useGLTF } from "@react-three/drei";
 
 export default function OceanModel() {
     const { nodes } = useGLTF("./ocean.glb");
-
     return (
-        <mesh
-            geometry={nodes.Ocean.geometry}
-            material={nodes.Ocean.material}
+        <primitive
+            object={nodes.Ocean}
             rotation={[-Math.PI / 2, 0, 0]}
         />
     );
