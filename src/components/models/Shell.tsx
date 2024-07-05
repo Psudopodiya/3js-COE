ae170a51aab656a67d64577c705da15a8fa5c030
suryamjain <suryam.j@consultadd.com>
2024-07-05 14:20:03 +0530

    feat(Shell): motion added to shell

--- src/components/models/Shell.tsx
+++ src/components/models/Shell.tsx
@@ -10,16 +10,17 @@ type Props = {
     rotation: THREE.Quaternion;
 };
 
-const dampingFactor = 0.9;
+const dampingFactor = 0.98;
 
 function Shell({ shellIndex, boatPosition, rotation }: Props): JSX.Element {
     const rigidBodyRef = useRef<RapierRigidBody | null>(null);
 
     const [position, setPosition] = useState<THREE.Vector3 | undefined>();
 
-    // Convert rotation quaternion to a direction vector
     const direction = new THREE.Vector3(0, 0, -1).applyQuaternion(rotation);
-    const initialVelocity = direction.multiplyScalar(1000);
+    const initialVelocity = direction
+        .multiplyScalar(500)
+        .add(new THREE.Vector3(0, 300, 0));
 
     const [velocity, setVelocity] = useState(initialVelocity);
     const sphere = new THREE.SphereGeometry(5, 32, 32);
@@ -30,9 +31,13 @@ function Shell({ shellIndex, boatPosition, rotation }: Props): JSX.Element {
 
     useFrame(() => {
         if (rigidBodyRef.current) {
-            rigidBodyRef.current?.setLinvel(velocity, true);
-            velocity.multiplyScalar(dampingFactor);
-            setVelocity(velocity.clone());
+            const gravity = new THREE.Vector3(0, -9.81, 0);
+            const newVelocity = velocity.clone().add(gravity);
+
+            newVelocity.multiplyScalar(dampingFactor);
+
+            rigidBodyRef.current.setLinvel(newVelocity, true);
+            setVelocity(newVelocity);
         }
     });
 
@@ -40,7 +45,16 @@ function Shell({ shellIndex, boatPosition, rotation }: Props): JSX.Element {
         if (boatPosition && rotation) {
             const updatedPosition = new THREE.Vector3();
             updatedPosition.copy(boatPosition);
-            updatedPosition.y += 20;
+
+            const backwardOffset = direction
+                .clone()
+                .normalize()
+                .multiplyScalar(-50);
+            updatedPosition.add(
+                new THREE.Vector3(backwardOffset.x, 0, backwardOffset.z),
+            );
+
+            updatedPosition.y += 30;
             setPosition(updatedPosition);
         }
     }, [boatPosition, rotation]);
